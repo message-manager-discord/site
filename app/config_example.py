6f42b97bcdb8e0db77c3eb077a869b51753688c8
AnotherCat <anothercat1259@gmail.com>
2020-12-24 00:08:47 +1300

    chore: add commands scope to default invite link

--- app/config_example.py
+++ app/config_example.py
@@ -3,7 +3,7 @@ secret_key = b""
 default_values = {
     "support_invite": "https://discord.gg/xFZu29t",
     "docs": "https://docs.messagemanager.xyz",
-    "invite": "https://discord.com/api/oauth2/authorize?client_id=735395698278924359&permissions=379968&scope=bot",
+    "invite": "https://discord.com/api/oauth2/authorize?client_id=735395698278924359&permissions=379968&scope=applications.commands%20bot",
     "privacy": "https://github.com/AnotherCat/message-bot/blob/master/PRIVACY_POLICY.md",
     "source": "https://github.com/AnotherCat/message-manager-site",
 }  # Change these values to yours
