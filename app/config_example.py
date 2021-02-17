50ee7c5fc6657949c1415c871fceb2c0158c4f16
AnotherCat <anothercat1259@gmail.com>
2021-02-17 20:29:16 +1300

    feat: add cloudflare analytcs script

--- app/config_example.py
+++ app/config_example.py
@@ -6,4 +6,5 @@ default_values = {
     "invite": "https://discord.com/api/oauth2/authorize?client_id=735395698278924359&permissions=537250880&scope=bot%20applications.commands",
     "privacy": "https://github.com/AnotherCat/message-bot/blob/master/PRIVACY_POLICY.md",
     "source": "https://github.com/AnotherCat/message-manager-site",
+    "cloudflare_analytics_token": "",
 }  # Change these values to yours
