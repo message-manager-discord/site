ab88e71042f4b4e7cbb7617d83cd3280f6440d31
Sam <anothercat1259@gmail.com>
2021-09-15 21:46:56 +1200

    feat: add next.js redirects for invite and privacy

--- next.config.js
+++ next.config.js
@@ -7,4 +7,18 @@ const withMDX = require("@next/mdx")({
 module.exports = withMDX({
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  async redirects() {
+    return [
+      {
+        source: '/privacy',
+        destination: 'https://github.com/message-manager-discord/bot/blob/master/PRIVACY_POLICY.md',
+        permanent: true,
+      },
+      {
+        source: '/invite',
+        destination: 'https://discord.com/api/oauth2/authorize?client_id=735395698278924359&permissions=537250880&scope=bot%20applications.commands',
+        permanent: true,
+      },
+    ]
+  },
 });
