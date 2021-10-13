02040d93ddc4d1ddf06a480f3b67da2bc5f5ca66
Sam <anothercat1259@gmail.com>
2021-10-14 11:25:52 +1300

    fix: next.js config

--- next.config.js
+++ next.config.js
@@ -6,23 +6,9 @@
 /** @type {import('next').NextConfig} */
 
 const { withSentryConfig } = require("@sentry/nextjs");
+const withPlugins = require("next-compose-plugins");
 const withMDX = require("@next/mdx")({
   extension: /\.mdx$/,
-});
-
-const moduleExports = withMDX({
-  reactStrictMode: true,
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  async redirects() {
-    return [
-      {
-        source: "/invite",
-        destination:
-          "https://discord.com/api/oauth2/authorize?client_id=735395698278924359&permissions=537250880&scope=bot%20applications.commands",
-        permanent: false,
-      },
-    ];
-  },
 });
 
 const SentryWebpackPluginOptions = {
@@ -37,6 +23,25 @@ const SentryWebpackPluginOptions = {
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
 
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
+module.exports = withPlugins([
+  [
+    withMDX,
+    {
+      reactStrictMode: true,
+      pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    },
+  ],
+  [withSentryConfig, SentryWebpackPluginOptions],
+  {
+    async redirects() {
+      return [
+        {
+          source: "/invite",
+          destination:
+            "https://discord.com/api/oauth2/authorize?client_id=735395698278924359&permissions=537250880&scope=bot%20applications.commands",
+          permanent: false,
+        },
+      ];
+    },
+  },
+]);
