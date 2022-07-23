04ca7b10bf2292ba5c1f41c0c93fe339e7ccbaec
AnotherCat <anothercat1259@gmail.com>
2022-07-23 22:00:05 +1200

    feat: create initial template - replace everything else

---
+++ app/root.tsx
@@ -0,0 +1,38 @@
+import type { MetaFunction } from "@remix-run/cloudflare";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import type { LinksFunction } from "@remix-run/cloudflare"; // or cloudflare/deno
+
+import styles from "./styles/app.css";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
