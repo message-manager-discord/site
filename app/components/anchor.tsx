f036374eeaf334456bf440f5e73af168121871b0
AnotherCat <anothercat1259@gmail.com>
2022-08-03 09:50:20 +1200

    fix: don't use any
    JS-0323

--- app/components/anchor.tsx
+++ app/components/anchor.tsx
@@ -1,19 +1,13 @@
 import classNames from "classnames";
 import { Link } from "@remix-run/react";
-
-type AnchorProps = {
-  to: string;
-  className?: string;
-  children: React.ReactNode;
-  [key: string]: any;
-};
+import type { RemixLinkProps } from "@remix-run/react/dist/components";
 
 export default function Anchor({
   className,
   to,
   children,
   ...props
-}: AnchorProps) {
+}: RemixLinkProps) {
   const classes = classNames(
     "border-b-2 border-slate-400 dark:border-slate-300 transition duration-300 ease-in-out hover:text-blue-500 dark:hover:text-blue-300",
     className
