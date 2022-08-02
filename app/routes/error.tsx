0818ff0a2f1868dc1ca93050abf929093e86a89d
AnotherCat <anothercat1259@gmail.com>
2022-08-02 21:00:52 +1200

    feat: sentry

---
+++ app/routes/error.tsx
@@ -0,0 +1,12 @@
+export default function a() {
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        throw new Error("Sentry Frontend Error");
+      }}
+    >
+      Throw error
+    </button>
+  );
+}
