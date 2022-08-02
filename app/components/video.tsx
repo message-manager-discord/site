3d3b75ae5b71240d645ba9b8ef6e583d8dc806a8
AnotherCat <anothercat1259@gmail.com>
2022-08-03 11:34:30 +1200

    fix: use `const` declarations for variables that are never reassigned
    JS-0242

--- app/components/video.tsx
+++ app/components/video.tsx
@@ -20,7 +20,7 @@ export default function Video({
   //ffmpeg -i fetch.mov -c:v libvpx-vp9 -pass 1 -b:v 1000K -threads 8 -speed 4 -tile-columns 6 -frame-parallel 1 -an -f webm /dev/null
   // ffmpeg -i fetch.mov -c:v libvpx-vp9 -pass 2 -b:v 1000K -threads 8 -speed 1 \\n  -tile-columns 6 -frame-parallel 1 -auto-alt-ref 1 -lag-in-frames 25 \\n  -c:a libopus -b:a 64k -f webm fetch.webm
 
-  let [mobile, setMobile] = useState(false);
+  const [mobile, setMobile] = useState(false);
 
   const shouldUseMobile = mobile && mobileSrc;
 
